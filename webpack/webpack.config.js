a2d35a046100d767cfd65de6a27c8aee31954a9c
yunamoon <tpwkao0051@gmail.com>
2024-03-07 16:00:41 +0900

    webpack html path, static config update;

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
     // 파일을 읽는 진입점 설정
     // webpack은 기본적으로 js 파일이 진입점
@@ -10,5 +11,22 @@ module.exports = {
         // path : path.resolve(__dirname, 'dist'),
         // filename : 'main.js',
         clean : true
+    },
+
+    // 번들링 후 결과물의 처리 방식 
+    plugins : [
+        new HtmlPlugin({
+            template : './index.html'
+        }),
+
+        new CopyPlugin({
+            patterns : [
+                {from : 'static'}
+            ]
+        })
+    ],
+
+    devServer : {
+        host : 'localhost'
     }
 }
